659dfe4ad57c4174ecacc9f23d7f815e82a49f34
Aleksey57266 <m4vseo@yandex.ru>
2025-03-05 20:47:34 +0300

    В locators добавил новый class. В login_pages описал новые проверки. В test_main_page добавил новый тест.

--- pages/login_pages.py
+++ pages/login_pages.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -9,12 +10,12 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert True
+        assert LoginPageLocators.LOGIN_IN_URL in self.browser.current_url, "в url нет login"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Отсутствует форма логина"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Отсутствует форма регистрации"
